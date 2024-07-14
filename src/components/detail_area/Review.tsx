5cbaaadfac592a81ba3c1ae45dc815c86400bd90
ichigo0126 <luckyshoma@gmail.com>
2024-07-15 05:14:36 +0900

    時間表記変更

--- src/components/detail_area/Review.tsx
+++ src/components/detail_area/Review.tsx
@@ -66,7 +66,9 @@ export default function Review({
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
-    return `${year}/${month}/${day}`;
+    const hour = String(date.getHours()).padStart(2, "0");
+    const minute = String(date.getMinutes()).padStart(2, "0");
+    return `${year}/${month}/${day} ${hour}:${minute}`;
   };
 
   // ブレークポイントに応じて幅を動的に設定
