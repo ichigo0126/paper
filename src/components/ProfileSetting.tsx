3104ee064596a4b852a815ff2f38f5fa932f09df
seiichiro <ps527execubel@gmail.com>
2024-07-15 01:28:31 +0900

    プロフィール画面にあるボタンによるページ遷移の実装

--- src/components/ProfileSetting.tsx
+++ src/components/ProfileSetting.tsx
@@ -13,95 +13,17 @@ import {
 import { CloseButton } from "@chakra-ui/react";
 import Header from "./detail_area/Header";
 import Profile from "./detail_area/Profile";
-
-type ContentType = {
-  name: string;
-  username: string;
-  description: string;
-  isFollowing: boolean;
-};
-
-function UserCard({ name, username, description, isFollowing }: ContentType) {
-  return (
-    <HStack spacing={5} alignItems="flex-start">
-      <Image
-        src="https://via.placeholder.com/68"
-        w="68px"
-        border="1px"
-        borderColor="gray.400"
-        borderRadius="md"
-      />
-      <VStack align="stretch" flex={1}>
-        <Flex justifyContent="space-between" alignItems="center">
-          <Text fontSize="lg" fontWeight="semibold">
-            {name} ({username})
-          </Text>
-          <Button size="sm" colorScheme={isFollowing ? "gray" : "green"}>
-            {isFollowing ? "フォロー済" : "フォロー"}
-          </Button>
-        </Flex>
-        <Text fontSize="md" fontWeight="light">
-          {description}
-        </Text>
-      </VStack>
-    </HStack>
-  );
-}
-
-const postContents = [
-  {
-    name: "秋田君",
-    username: "katikati_yane",
-    description:
-      "本と記事のレビューが大好きな人、集まれ！新しい作品を発見し、レビューして、いいね＆ブックマークで応援しよう！",
-    isFollowing: true,
-  },
-  {
-    name: "秋田君_1",
-    username: "katikati_yan",
-    description:
-      "読書好きの集まる場所でレビューを書いてシェアしましょう！面白い作品に出会ったら、いいね＆ブックマークもよろしく！",
-    isFollowing: false,
-  },
-  {
-    name: "秋田君_2",
-    username: "katikati",
-    description:
-      "本と記事のレビューを通じて、読書仲間とつながる場所です。あなたのおすすめ作品をレビューして共有し、他のユーザーと感想を交換しょう。気に入った作品にはいいねを、また読みたい作品にはブックマークを！一緒に読書の楽しさを広げましょう。",
-    isFollowing: false,
-  },
-  {
-    name: "秋田君_3",
-    username: "katikati",
-    description:
-      "本と記事のレビューが大好きな人、集まれ！新しい作品を発見し、レビューして、いいね＆ブックマークで応援しよう！",
-    isFollowing: true,
-  },
-  {
-    name: "秋田君_4",
-    username: "katikati",
-    description:
-      "本と記事が大好きな読書家。レビューを書いて、他の読書好きとつながろう！お気に入りの作品を見つけて、いいね＆ブックマークも忘れずに。",
-    isFollowing: true,
-  },
-  {
-    name: "秋田君_5",
-    username: "katikati",
-    description:
-      "読書コミュニティの一員として、レビューを投稿して感想を共有しましょう。みんなのおすすめ作品にいいねとブックマークを！",
-    isFollowing: false,
-  },
-];
+import { Link } from "react-router-dom";
 
 const profile = {
   name: "杉本大志",
   username: "kachikachichinko",
-  reviewCount: 2.000,
-  valueCount: 10.000,
+  reviewCount: 2.0,
+  valueCount: 10.0,
   description: "かちかちちんこ",
-  followCount: 2.000,
-  followedCount: 3.000
-}
+  followCount: 2.0,
+  followedCount: 3.0,
+};
 
 function FollowPage() {
   const isMobile = useBreakpointValue({ base: true, md: false });
@@ -121,8 +43,15 @@ function FollowPage() {
           />
           <Box w={isMobile ? "full" : "69%"}>
             <Box p={4} pb={20} bg="gray.50" borderRadius="3xl" shadow="sm">
+              <Flex justifyContent="space-between" alignItems="center">
+                <Text fontSize="lg" fontWeight="light">
+                  フォロー 1,301
+                </Text>
+                <Link to="/home/mypage">
+                  <CloseButton />
+                </Link>
+              </Flex>
               <Divider my={2} borderWidth="2px" borderColor="gray.500" />
-             
             </Box>
           </Box>
         </Flex>
