db27c6f6c2d086e5edae56c43278edba9ed44a8b
ichigo0126 <luckyshoma@gmail.com>
2024-07-18 03:04:31 +0900

    MyLikePage型エラー修正

--- src/components/MyLikePage.tsx
+++ src/components/MyLikePage.tsx
@@ -8,10 +8,21 @@ import {
 } from "@chakra-ui/react";
 import Review from "./detail_area/Review";
 import { useLike } from '../components/LikeContext';
+import { useEffect, useState } from 'react';
+import { auth } from '../firebase'; // Firebaseの設定ファイルからauthをインポート
 
 function MyLikePage() {
   const isMobile = useBreakpointValue({ base: true, md: false });
   const { likedReviews } = useLike();
+  const [currentUser, setCurrentUser] = useState<any>(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   return (
     <Box pt={2.5} pb={4} bg="gray.100" borderRadius="normal" h="100vh">
@@ -20,7 +31,13 @@ function MyLikePage() {
           <VStack spacing={4} align="stretch" w="full">
             {likedReviews.length > 0 ? (
               likedReviews.map((review) => (
-                <Review key={review.id} {...review} />
+                <Review 
+                  key={review.id} 
+                  {...review} 
+                  name={review.username || "Unknown"}
+                  photoURL={review.photoURL || null}
+                  currentUsername={currentUser?.displayName || ""}
+                />
               ))
             ) : (
               <Text>いいねしたレビューはまだありません。</Text>
